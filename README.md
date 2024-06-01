34c81c742090a7ee064967b117c13eb02c85c28c
Balsa-Dogandzic <100625564+Balsa-Dogandzic@users.noreply.github.com>
2024-06-01 11:13:22 +0200

    Precriptions page functionalities

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
 # Online-pharmacy
 
-## Odradjeno za sad:
+## Sifre
 
-### Frontend:
-1. Pocetna stranica
-2. Stranica sa ljekovima
-3. Korpa
+admin
+Admin123
 
-### Backend:
-1. Napravljena baza
-2. Ostvarena konekcija sa bazom
-3. Povezana index stranica
-
-## TODO
-1. Dijagram sekvence, korisnicki zahtjevi i opis projekta su potrebni za izvjestaj
-2. Uvezivanje svake od stranica sa backend-om i prikazivanje podataka iz baze na frontend-u
-3. Posebna stranica za pojedinacni proizvod, about us, stranice login i register za sada.
+pero
+Pero123
--- app/main/medicine.py
+++ app/main/medicine.py
@@ -1,6 +1,7 @@
-from flask import render_template, session, redirect
+from flask import render_template, session, redirect, request
 from app.main import bp
 from app import mysql
+from datetime import datetime
 
 @bp.route('/ljekovi/')
 def all_medicine():
@@ -27,12 +28,88 @@ def medicine(id):
 
 @bp.route('/ljekovi/dodaj/')
 def add_medicine():
+    if session.get("user_type") != 2:
+        return redirect("/ljekovi/")
     return "Work in progress..."
 
 
-@bp.route('/recepti/')
+@bp.route('/ljekovi/promijeni/<int:id>/')
+def update_medicine(id):
+    if session.get("user_type") != 2:
+        return redirect("/ljekovi/")
+    return "Work in progress..."
+
+
+@bp.route('/ljekovi/obrisi/<int:id>/')
+def delete_medicine(id):
+    if session.get("user_type") != 2:
+        return redirect("/ljekovi/")
+    return "Work in progress..."
+
+
+@bp.route('/recepti/', methods=['GET', 'POST'])
 def prescriptions():
+    if not session.get("user"):
+        return redirect("/ljekovi/")
+    if request.method == 'POST':
+        prescription = request.form['prescription']
+        return redirect(f'/recepti/?recept={prescription}')
+    prescription = request.args.get('recept')
     cursor = mysql.connection.cursor()
     cursor.execute(f"SELECT * FROM prescription WHERE user_id={session.get('user')}")
     prescriptions = cursor.fetchall()
+    if prescription:
+        cursor.execute(f"SELECT therapy.*, medicine.name, medicine.image FROM medicine JOIN therapy ON therapy.medicine_id = medicine.id WHERE prescription_id={prescription}")
+        therapy = cursor.fetchall()
+        prescription_id  = therapy[0].get('prescription_id')
+        return render_template("prescriptions.html", prescriptions=prescriptions, therapy=therapy, id=prescription_id)
     return render_template("prescriptions.html", prescriptions=prescriptions)
+
+
+@bp.route('/recepti/dodaj/', methods=['GET', 'POST'])
+def add_prescriptions():
+    if not session.get("user"):
+        return redirect("/ljekovi/")
+    if request.method == 'POST':
+        prescription = request.form['prescription']
+        today_date = datetime.now().strftime('%Y-%m-%d')
+        cursor = mysql.connection.cursor()
+        cursor.execute(f"INSERT INTO prescription VALUES (NULL, '{prescription}', '{today_date}', '{session.get('user')}')")
+        mysql.connection.commit()
+        cursor.close() 
+    return redirect('/recepti/')
+
+
+@bp.route('/recepti/obrisi/<int:id>')
+def delete_prescription(id):
+    if not session.get("user"):
+        return redirect("/ljekovi/")
+    cursor = mysql.connection.cursor()
+    cursor.execute(f"DELETE FROM prescription WHERE id={id} AND user_id={session.get('user')}")
+    mysql.connection.commit()
+    return redirect('/recepti/')
+
+
+@bp.route('/terapija/dodaj/', methods=['GET', 'POST'])
+def add_therapy():
+    if not session.get("user"):
+        return redirect("/ljekovi/")
+    if request.method == 'POST':
+        medicine = request.form['medicine']
+        prescription = request.form['prescription']
+        cursor = mysql.connection.cursor()
+        cursor.execute(f"INSERT INTO therapy VALUES (NULL, '{prescription}', '{medicine}', 1)")
+        mysql.connection.commit()
+        cursor.close() 
+        return redirect(f'/recepti/?recept={prescription}')
+    return redirect('/recepti/')
+
+
+@bp.route('/terapija/obrisi/<int:id>/', methods=['GET', 'POST'])
+def remove_therapy(id):
+    if not session.get("user"):
+        return redirect("/ljekovi/")
+    cursor = mysql.connection.cursor()
+    cursor.execute(f"DELETE FROM therapy WHERE id={id}")
+    mysql.connection.commit()
+    return redirect(request.referrer)
